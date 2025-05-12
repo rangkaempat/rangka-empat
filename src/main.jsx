6eed8919dc1eceb1fa5c0ce805ce03e9e0c93d53
Nik-at-262992094974 <nikuyunfurqan@gmail.com>
2025-05-12 17:59:08 +0800

    merged

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { HashRouter, Routes, Route } from "react-router-dom"; // Correct import for react-router-dom
+import { HashRouter, Routes, Route } from "react-router"; // Correct import for react-router-dom
 import "./index.scss";
 import Hero from "./pages/home/Hero.jsx";
 import ScrollToTop from "./functions/scrollToTop.jsx";
