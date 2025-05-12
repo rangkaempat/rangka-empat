ba5039238880e67ffc3ac76650caa5d72a054905
Nik-at-262992094974 <nikuyunfurqan@gmail.com>
2025-05-12 17:35:52 +0800

    Navbar Prototype

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter, BrowserRouter, Routes, Route } from "react-router";
-import './index.scss'
+import "./index.scss";
 
-import ScrollToTop from "./ScrollToTop.jsx";
+import ScrollToTop from "./functions/scrollToTop.jsx";
 import Home from "./pages/home/Home.jsx";
 
 export default function App() {
   return (
-
     // Change from HashRouter to BrowserRouter when deploying other than GitHub Pages
     // GitHub Pages does not support BrowserRouter, so use HashRouter instead
     <HashRouter>
-      
       {/* Function to Scroll To Top of Page for React-Router when changing pages */}
       <ScrollToTop />
-      
+
       <Routes>
         {/* Layout = Navbar */}
         {/* <Route path="/" element={<Layout />} /> */}
 
         <Route index element={<Home />} />
-
       </Routes>
-
     </HashRouter>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,31 @@
-import {React , useEffect} from "react";
-import {Outlet, Link} from "react-router";
-
+import { React, useEffect } from "react";
+import { Outlet, Link } from "react-router";
+import "../../index.scss";
+import "./Home.scss";
 export default function Home() {
   useEffect(() => {
-    document.title = "Rangka Empat Studio | We Design. We Code. We Deliver. We Care."; // Quick solution
+    document.title =
+      "Rangka Empat Studio | We Design. We Code. We Deliver. We Care."; // Quick solution
   }, []);
 
   return (
     <>
-      {/* <Link> </Link> for different pages */}
-      <Link to="/">Home</Link>
-
-      <div>Hello</div>
-
-      <Outlet />
+      <div className="navbarWrapper">
+        <div className="mainWrapper">
+          <div className="navbar">
+            <img
+              className="navlogo"
+              src="././src/assets/rangka-empat-studio-logo.webp"
+            />
+            <div className="navlinkContainer">
+              <div className="navlink">About</div>
+              <div className="navlink">Services</div>
+              <div className="navlink">Our Work</div>
+            </div>
+            <button className="navButton">Start Your Project</button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
