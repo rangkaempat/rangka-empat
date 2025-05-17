601acb0a138567a5a7ba678e4245ee94307832ca
Rangka Empat Studio <rangkaempatstudio@gmail.com>
2025-05-18 00:16:33 +0800

    Modify Footer animations and style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,102 +4,168 @@ import { Outlet, Link } from "react-router";
 import Button from "../button/Button";
 import { motion } from "framer-motion";
 import footerLogo from "/src/assets/rangka-empat-studio-logo-footer.webp";
+import logo from "/src/assets/rangka-empat-studio-logo.webp";
 import facebooklogo from "/src/assets/logoFacebook.webp";
 import instagramlogo from "/src/assets/logoInstagram.webp";
 import linkedinlogo from "/src/assets/logoLinkedin.webp";
 import tiktoklogo from "/src/assets/logoTiktok.webp";
+import homeHeroBg from "/src/assets/home-hero-bg.webp";
+import { fadeInWithEase, staggerContainer } from "../../functions/motionUtils";
 
-import { fadeInWithEase } from "../../functions/motionUtils";
+const footerLinks = [
+  { name: "Home", to: "/" },
+  { name: "About", to: "/about" },
+  { name: "Services", to: "/services" },
+  { name: "Our Work", to: "/our-work" },
+  { name: "Pricing", to: "/pricing" },
+];
+
+const socialLinks = [
+  {
+    name: "Instagram",
+    to: "https://www.instagram.com/rangkaempatstudio",
+    logo: instagramlogo,
+  },
+  {
+    name: "Facebook",
+    to: "https://www.facebook.com/rangkaempatstudio",
+    logo: facebooklogo,
+  },
+  {
+    name: "TikTok",
+    to: "https://www.tiktok.com/rangkaempat",
+    logo: tiktoklogo,
+  },
+  {
+    name: "LinkedIn",
+    to: "https://www.linkedin.com/company/rangka-empat-studio",
+    logo: linkedinlogo,
+  },
+];
+
 export default function Footer() {
   return (
     <>
-      <div className="footerBackground">
-        <div className="footerContainer">
-          {/* footerlinks */}
-          <div className="footerlinkContainer">
-            <Link className="footerlink" to="/about">
-              Home
-            </Link>
-            <Link className="footerlink" to="/about">
-              About
-            </Link>
-            <Link className="footerlink" to="#">
-              Services
-            </Link>
-            <Link className="footerlink" to="#">
-              Our Work
-            </Link>
-            <Link className="footerlink" to="#">
-              Testimonials
-            </Link>
-            <Link className="footerlink" to="#">
-              Pricing
-            </Link>
+      <div
+        className="sectionBackground"
+        style={{ backgroundImage: `url(${homeHeroBg})` }}
+      >
+        <div className="sectionBackgroundBlur">
+          <div className="sectionWrapper">
+            <div className="sectionContent footerContent">
+              {/* Footer Links */}
+              <motion.div
+                className="footerlinkContainer"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.2 }}
+                variants={staggerContainer}
+              >
+                <motion.h5 className="footerlink" variants={fadeInWithEase}>
+                  Quick Links
+                </motion.h5>
+
+                {footerLinks.map((link, index) => (
+                  <motion.div key={index} variants={fadeInWithEase}>
+                    <Link className="footerlink" to={link.to}>
+                      {link.name}
+                    </Link>
+                  </motion.div>
+                ))}
+              </motion.div>
+
+              {/* Footer Centre */}
+              <motion.div
+                className="footerCentre"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.2 }}
+                variants={staggerContainer}
+              >
+                <motion.div variants={fadeInWithEase}>
+                  <Link to="/">
+                    <img
+                      className="footerlogo"
+                      src={footerLogo}
+                      alt="Rangka Empat Studio Logo"
+                    />
+                  </Link>
+                </motion.div>
+                <motion.h1 className="footerh1" variants={fadeInWithEase}>
+                  We Design. <span>We Code.</span>
+                  <br />
+                  We Deliver. <span>We Care.</span>
+                </motion.h1>
+              </motion.div>
+
+              {/* Footer Socials */}
+              <motion.div
+                className="footerlinkContainerLogo"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.2 }}
+                variants={staggerContainer}
+              >
+                {socialLinks.map((link, index) => (
+                  <motion.div key={index} variants={fadeInWithEase}>
+                    <Link
+                      className="footerlink"
+                      to={link.to}
+                      target="_blank"
+                      rel="noopener"
+                    >
+                      <img
+                        className="footerlogo"
+                        src={link.logo}
+                        alt={`${link.name} Logo`}
+                      />
+                    </Link>
+                  </motion.div>
+                ))}
+              </motion.div>
+              {/* footer Logo */}
+            </div>
           </div>
-          <div className="footerCentre">
-            <Link to="/">
-              <img
-                className="footerlogo"
-                src={footerLogo}
-                alt="Rangka Empat Studio Logo"
-              />
-            </Link>
-            <motion.h1 className="footerh1" variants={fadeInWithEase}>
-              We Design. <span>We Code.</span>
-              <br />
-              We Deliver. <span>We Care.</span>
-            </motion.h1>
-          </div>
-          <div className="footerlinkContainerLogo">
-            <Link className="footerlink" to="/about">
-              <img
-                className="footerlogo"
-                src={instagramlogo}
-                alt="Rangka Empat Studio Logo"
-              />
-            </Link>
-            <Link className="footerlink" to="/about">
-              <img
-                className="footerlogo"
-                src={facebooklogo}
-                alt="Rangka Empat Studio Logo"
-              />
-            </Link>
-            <Link className="footerlink" to="#">
-              <img
-                className="footerlogo"
-                src={tiktoklogo}
-                alt="Rangka Empat Studio Logo"
-              />
-            </Link>
-            <Link className="footerlink" to="#">
-              <img
-                className="footerlogo"
-                src={linkedinlogo}
-                alt="Rangka Empat Studio Logo"
-              />{" "}
-            </Link>
-          </div>
-          {/* footer Logo */}
-        </div>
-        <div className="footerContainer">
-          <div className="footerlinkBottom">
-            <Link className="footerlink" to="/about">
-              Sitemap
-            </Link>
-            <span>•</span>
-            <Link className="footerlink" to="/about">
-              Privacy Policy
-            </Link>
-            <span>•</span>
-            <Link className="footerlink" to="#">
-              Cookies
-            </Link>
-          </div>
-          <div className="footerCopyright">
-            © 2025 Rangka Empat Studio (BRN)
-            <br></br>
-            All Rights Reserved
+          <div className="sectionWrapper">
+            <div className="sectionContent footerContent">
+              <motion.div
+                className="footerlinkBottom"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.2 }}
+                variants={staggerContainer}
+              >
+                <motion.div variants={fadeInWithEase}>
+                  <Link className="footerlink" to="/sitemap">
+                    Sitemap
+                  </Link>
+                </motion.div>
+                <span>•</span>
+                <motion.div variants={fadeInWithEase}>
+                  <Link className="footerlink" to="/privacy-policy">
+                    Privacy Policy
+                  </Link>
+                </motion.div>
+                <span>•</span>
+                <motion.div variants={fadeInWithEase}>
+                  <Link className="footerlink" to="cookies">
+                    Cookies
+                  </Link>
+                </motion.div>
+              </motion.div>
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.2 }}
+                variants={staggerContainer}
+              >
+                <motion.p className="footerCopyright" variants={fadeInWithEase}>
+                  &copy; {new Date().getFullYear()} Rangka Empat Studio (BRN)
+                  <br></br>
+                  All Rights Reserved
+                </motion.p>
+              </motion.div>
+            </div>
           </div>
         </div>
       </div>
