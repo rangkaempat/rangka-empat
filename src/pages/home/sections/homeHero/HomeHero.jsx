3724d65f7931c228e7cdac57445cfc79b9421818
Rangka Empat Studio <rangkaempatstudio@gmail.com>
2025-05-18 16:34:22 +0800

    Modify Home Hero

--- src/pages/home/sections/homeHero/HomeHero.jsx
+++ src/pages/home/sections/homeHero/HomeHero.jsx
@@ -3,6 +3,7 @@ import "./HomeHero.scss";
 import { Link, Outlet } from "react-router";
 import Button from "../../../../components/button/Button";
 import homeHeroBg from "/src/assets/home-hero-bg.webp";
+import homeHeroBg2 from "/src/assets/home-hero-bg-2.webp";
 import { motion } from "framer-motion";
 
 import clientLogo1 from "/src/assets/clientLogo1.webp";
@@ -14,6 +15,7 @@ import {
   infiniteLoop,
   staggerContainer,
 } from "../../../../functions/motionUtils";
+import { workCardItems } from "../../../../data/workCardData";
 
 const clientLogos = [clientLogo1, clientLogo2, clientLogo3];
 
@@ -21,12 +23,12 @@ export default function HomeHero() {
   return (
     <>
       <section
-        className="sectionBackground"
-        style={{ backgroundImage: `url(${homeHeroBg})` }}
+        className="sectionBackground homeHeroBackground"
+        style={{ backgroundImage: `url(${homeHeroBg2})` }}
       >
         <div className="sectionBackgroundBlur homeHeroBackgroundGradient">
           <div className="sectionWrapper homeHeroWrapper">
-            <div className="sectionContent">
+            <div className="sectionContent homeHeroContent">
               {/* Hero Title */}
               <motion.div
                 className="homeHeroTitle"
@@ -44,27 +46,74 @@ export default function HomeHero() {
                   Crafting custom, beautiful, high-converting SEO-Optimized
                   digital experiences that connect brands with their audiences.
                 </motion.p>
+
+                {/* Hero Buttons */}
+                <div className="homeHeroBtnContainer">
+                  <Button
+                    name="Start Your Project"
+                    type="btnType1"
+                    link="#"
+                    arrow={true}
+                  />
+                  <Button
+                    name="View Our Work"
+                    type="btnType2"
+                    link="#"
+                    arrow={true}
+                  />
+                </div>
               </motion.div>
 
-              {/* Hero Description */}
+              <motion.div
+                className="homeCTAImageContainer"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.2 }}
+                variants={staggerContainer}
+              >
+                <motion.div
+                  className="workCardContainer "
+                  key={workCardItems[0].id}
+                  variants={fadeInWithEase}
+                >
+                  <img
+                    className="workImage"
+                    src={workCardItems[0].image}
+                    alt={`${workCardItems[0].client} Image`}
+                  />
+                </motion.div>
+                <motion.div
+                  className="workCardContainer"
+                  key={workCardItems[1].id}
+                  variants={fadeInWithEase}
+                >
+                  <img
+                    className="workImage"
+                    src={workCardItems[1].image}
+                    alt={`${workCardItems[1].client} Image`}
+                  />
+                </motion.div>
+                <motion.div
+                  className="workCardContainer homeCTAImage"
+                  key={workCardItems[2].id}
+                  variants={fadeInWithEase}
+                >
+                  <img
+                    className="workImage"
+                    src={workCardItems[2].image}
+                    alt={`${workCardItems[2].client} Image`}
+                  />
+                </motion.div>
+              </motion.div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-              {/* Hero Buttons */}
-              <div className="homeHeroBtnContainer">
-                <Button
-                  name="Start Your Project"
-                  type="btnType1"
-                  link="#"
-                  arrow={true}
-                />
-                <Button
-                  name="View Our Work"
-                  type="btnType2"
-                  link="#"
-                  arrow={true}
-                />
-              </div>
-
-              {/* Clientele Section */}
+      <section className="sectionLight">
+        <div className="sectionBackgroundBlurt">
+          <div className="sectionWrapper">
+            <div className="sectionContent">
               <motion.div
                 className="sectionContent"
                 initial="hidden"
@@ -93,7 +142,8 @@ export default function HomeHero() {
                           className="clientLogo"
                           src={logo}
                           alt="Client Logo"
-                          whileHover={{ y: -10 }}
+                          whileHover={{ y: -10, opacity: 0.5 }}
+                          whileTap={{ y: -10, opacity: 0.5 }}
                           transition={{ type: "spring", stiffness: 200 }}
                         />
                       ))}
