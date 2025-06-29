e1533e8586c350a38af377a3e23a40d13e84f5d6
Rangka Empat Studio <rangkaempatstudio@gmail.com>
2025-06-30 00:22:57 +0800

    Modify About page

--- src/pages/about/sections/aboutWhoWeAre/AboutWhoWeAre.jsx
+++ src/pages/about/sections/aboutWhoWeAre/AboutWhoWeAre.jsx
@@ -4,6 +4,13 @@ import { ThemeContext } from "../../../../functions/themeContext";
 
 import image1 from "/src/assets/aboutImages/aboutImage1.webp";
 import image2 from "/src/assets/aboutImages/aboutImage2.webp";
+import image3 from "/src/assets/aboutImages/aboutImage3.webp";
+import image4 from "/src/assets/aboutImages/aboutImage4.webp";
+import { motion } from "framer-motion";
+import {
+  fadeInWithEase,
+  staggerContainer,
+} from "../../../../functions/motionUtils";
 
 function AboutWhoWeAre() {
   const { darkMode } = useContext(ThemeContext);
@@ -11,37 +18,90 @@ function AboutWhoWeAre() {
   return (
     <>
       <section className={darkMode ? "sectionDark" : "sectionLight"}>
-        <div className="sectionContent aboutWhoWeAreContent">
+        <motion.div
+          className="sectionContent aboutWhoWeAreContent"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+          variants={staggerContainer}
+        >
           <div className="aboutWhoWeAreText">
-            <h2 className="textBold textXXXL">WHO WE ARE:</h2>
-            <p className="textLight textM">
-              <span className="textBold">We’re Rangka Empat Studio</span> — a
-              small, close-knit team of designers, developers, and
-              problem-solvers who genuinely love what we do.
+            <motion.h2 className="textBold textXXXL" variants={fadeInWithEase}>
+              WHO WE ARE:
+            </motion.h2>
+
+            <div className="aboutWhoWeAreTextRight">
+              <div className="aboutWhoWeAreCarouselWrapper">
+                <motion.img
+                  src={image1}
+                  alt="About Us Image 1"
+                  variants={fadeInWithEase}
+                />
+                <motion.img
+                  src={image3}
+                  alt="About Us Image 2"
+                  variants={fadeInWithEase}
+                />
+                <motion.img
+                  src={image2}
+                  alt="About Us Image 1"
+                  variants={fadeInWithEase}
+                />
+              </div>
+              <motion.p
+                className="textLight textM"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.2 }}
+                variants={staggerContainer}
+              >
+                <motion.span className="textBold" variants={fadeInWithEase}>
+                  We’re Rangka Empat Studio
+                </motion.span>{" "}
+                <motion.span variants={fadeInWithEase}>
+                  — a small, close-knit team of designers, developers, and
+                  problem-solvers who genuinely love what we do.
+                  <br />
+                  <br />
+                  We’re friends who grew up together, studied together, and
+                  shared a dream{" "}
+                </motion.span>
+                <motion.span className="textBold" variants={fadeInWithEase}>
+                  to build something meaningful with people we trust and
+                  respect.
+                </motion.span>
+              </motion.p>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div
+          className="sectionWrapper"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+          variants={staggerContainer}
+        >
+          <div className="sectionContent">
+            <motion.h2 className="textLight textS" variants={fadeInWithEase}>
+              That dream turned into <br />
               <br />
-              <br />
-              We’re friends who grew up together, studied together, and shared a
-              dream{" "}
-              <span className="textBold">
-                to build something meaningful with people we trust and respect.
+              <span
+                className="textBold textXXXL textMask"
+                style={{ backgroundImage: `url(${image4})` }}
+              >
+                RANGKA EMPAT STUDIO
               </span>
-              <br />
-              <br />
-              That dream turned into Rangka Empat Studio —{" "}
+            </motion.h2>
+            <motion.p className="textLight textS" variants={fadeInWithEase}>
               <span className="textBold">
-                a creative branding, web design and web development studio
+                A creative branding, web design and web development studio
               </span>{" "}
               focused on helping brands grow through thoughtful design, smart
               development, and real human connection.
-            </p>
+            </motion.p>
           </div>
-
-          <div className="aboutWhoWeAreCarouselWrapper">
-            <img src={image1} alt="About Us Image 1" />
-            <img src={image2} alt="About Us Image 2" />
-            <img src={image1} alt="About Us Image 1" />
-          </div>
-        </div>
+        </motion.div>
       </section>
     </>
   );
