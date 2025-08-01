4236aaae1aae11aca9d33ed139f76e902c7cd8fd
Nik-at-262992094974 <nikuyunfurqan@gmail.com>
2025-08-01 09:39:40 +0800

    fix services

--- src/components/servicesCard/RelatedProjectCard.jsx
+++ src/components/servicesCard/RelatedProjectCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { fadeInWithEase } from "../../functions/motionUtils";
 import { Link } from "react-router";
 import { motion } from "framer-motion";
-import "./servicesCard.scss";
+import "./ServicesCard.scss";
 
 function RelatedProjectCard({ image, number, title, description, link, tags }) {
   return (
